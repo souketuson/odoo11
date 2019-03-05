8ed13d56f553ca38cae8b267d54c122c8bc9b52f
ARJhe <meopkids@gmail.com>
2019-03-05 18:15:46 +0800

    add some cols

--- addons_yc/yc_produce/models/produce.py
+++ addons_yc/yc_produce/models/produce.py
@@ -3,14 +3,13 @@
 
 from odoo import models, fields, api
 from datetime import datetime as dt
-from time import gmtime, strftime
 
 
 class YcWeight(models.Model):
     _name = "yc.weight"
 
     driver_id = fields.Many2one("yc.driver", string="司機名稱")
-    name = fields.Char("過磅單號", default=lambda self: self.env["ir.sequence"].next_by_code("WeightList.sequence"))
+    name = fields.Char("過磅單號",  default=lambda self: self.env["ir.sequence"].next_by_code("WeightList.sequence"))
 
     # 要改成自動編號 & 上鎖
     @api.model
@@ -18,40 +17,41 @@ class YcWeight(models.Model):
         '''WL + 190227 + 001...999
                     2    +  6          + 3
                     '''
-        # prefix WL + yy-mm-dd
+        # prefix WL + yymmdd
         _serial = 'WL' + dt.today().strftime("%y%m%d")
-        # search db of today's last one data
+        # search today's last one data on db
+        _next = 0
         obj = self.env['yc.weight'].search([('name', '=like', _serial + "%")], limit=1, order='name DESC')
-        if obj:  # 如果有系列碼
+        if obj:  # 如果無/有系列碼
             _next = int(obj[0].name[8:]) + 1
             _serial += '%03d' % _next
-        elif not obj:
+        else:
             _serial += '001'
-
         self.name = _serial
 
     day = fields.Date("過磅日期", default=dt.today())
 
-    weightime = fields.Datetime("過磅時間", default=lambda self: dt.strptime(strftime("%Y-%m-%d %H:%M:%S", gmtime()),
-                                                                         "%Y-%m-%d %H:%M:%S"))
+    weightime = fields.Datetime("過磅時間", default=lambda self:  dt.strptime(dt.now().strftime("%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S"))
     person_id = fields.Many2one("yc.hr", string="過磅員")
-    car_no = fields.Char("車次序號", default=lambda self: self.env["ir.sequence"].next_by_code("ordinal.code"))
-    in_out = fields.Selection([('i', '進貨'), ('o', '出貨')], '進出貨')
-    factory = fields.Many2one("yc.factory", string="所屬工廠")
+    weighbridge = fields.Char("地磅序號")
+    carno = fields.Char("車次序號", default=lambda self: self.env["ir.sequence"].next_by_code("ordinal.code"))
+    in_out = fields.Selection([('I', '進貨'), ('O', '出貨')], '進出貨')
+    factory_id = fields.Many2one("yc.factory", string="所屬工廠")
 
     purchase_times = fields.Integer("進貨次數", compute="_count", store=True)
     ship_times = fields.Integer("出貨次數", compute="_count", store=True)
 
     # 進出貨次數要改成自動記錄
+    @api.multi
     @api.depends('in_out')
     def _count(self):
         for rec in self:
             check_day = dt.strptime(rec.day, "%Y-%m-%d")
             pn = rec.plate_no
             check_in = rec.env["yc.weight"].search(
-                [('in_out', '=', 'i'), ('day', '=', check_day), ('plate_no', '=', pn)])
+                [('in_out', '=', 'I'), ('day', '=', check_day), ('plate_no', '=', pn)])
             check_out = rec.env["yc.weight"].search(
-                [('in_out', '=', 'o'), ('day', '=', check_day), ('plate_no', '=', pn)])
+                [('in_out', '=', 'O'), ('day', '=', check_day), ('plate_no', '=', pn)])
             if rec.in_out:  # 進出貨有值
                 if pn and rec.day:  # 車牌&日期 有值
                     rec.purchase_times = len(check_in)
@@ -67,25 +67,25 @@ class YcWeight(models.Model):
             raise Warning("進出貨分類空值")
 
     plate_no = fields.Char("車號", required=True)
-    total_weight = fields.Integer("總重 (KG)")
-    curb_weight = fields.Integer("空車重 (KG)")
-    ept_buc_weight = fields.Integer("空桶重 (KG)")
-    net_weight = fields.Integer("淨重 (KG)", compute="_NetWeight")
+    total = fields.Integer("總重 (KG)")
+    curbweight = fields.Integer("空車重 (KG)")
+    other = fields.Integer("其他重量 (KG)")
+    emptybucket = fields.Integer("空桶重 (KG)")
+    net = fields.Integer("淨重 (KG)", compute="_NetWeight")
+    note = fields.Char("備註")
 
     # 改成自動計算
 
     def _NetWeight(self):
-        self.net_weight = self.total_weight - self.curb_weight - self.ept_buc_weight
+        self.net = self.total - self.curbweight - self.emptybucket
 
-    refine_weight = fields.Integer("調質重量 (KG)")
-    carburize_weight = fields.Float("滲碳單價")
-    other1 = fields.Char("其他1")
-    other2 = fields.Char("其他2")
+    refine = fields.Integer("調質重量 (KG)")
+    carbur = fields.Float("滲碳單價")
+    other1 = fields.Integer("其他重量1")
+
 
     # 一張過磅單 上面的貨物可能含有多家客戶
     customer_detail_ids = fields.One2many("yc.weight.details", "name", "客戶明細")
-    # def form_time(self):
-    # 檢查欄位的形式是否符合 XX:XX
 
 
 class YcWeightDetails(models.Model):
