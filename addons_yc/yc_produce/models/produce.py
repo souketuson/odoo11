3d1dacec2b31ec3aceeb93d7ce019e68de2b5b51
ARJhe <meopkids@gmail.com>
2019-03-06 16:36:33 +0800

    solve ship&purchase_times problem

--- addons_yc/yc_produce/models/produce.py
+++ addons_yc/yc_produce/models/produce.py
@@ -33,7 +33,8 @@ class YcWeight(models.Model):
 
     day = fields.Date("過磅日期", default=dt.today())
 
-    weightime = fields.Datetime("過磅時間", default=lambda self:  dt.strptime(dt.now().strftime("%Y-%m-%d %H:%M:%S"),"%Y-%m-%d %H:%M:%S"))
+    weightime = fields.Datetime("過磅時間", default=lambda self: dt.strptime(dt.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                                                         "%Y-%m-%d %H:%M:%S"))
     person_id = fields.Many2one("yc.hr", string="過磅員")
     weighbridge = fields.Char("地磅序號")
     carno = fields.Char("車次序號")
@@ -49,7 +50,7 @@ class YcWeight(models.Model):
     purchase_times = fields.Integer("進貨次數", compute="_count", store=True)
     ship_times = fields.Integer("出貨次數", compute="_count", store=True)
 
-    # 進出貨次數要改成自動記錄
+    # 進出貨次數自動記錄
     @api.multi
     @api.depends('in_out')
     def _count(self):
@@ -62,8 +63,12 @@ class YcWeight(models.Model):
                 [('in_out', '=', 'O'), ('day', '=', check_day), ('plate_no', '=', pn)])
             if rec.in_out:  # 進出貨有值
                 if pn and rec.day:  # 車牌&日期 有值
-                    rec.purchase_times = len(check_in)
-                    rec.ship_times = len(check_out)
+                    if rec.in_out == 'I':
+                        self.ship_times = len(check_out)
+                        self.purchase_times = len(check_in) + 1
+                    elif rec.in_out == 'O':
+                        self.ship_times = len(check_out) + 1
+                        self.purchase_times = len(check_in)
                 else:  # 進出貨 空值
                     pass
             else:
@@ -74,15 +79,16 @@ class YcWeight(models.Model):
         if not self.in_out:
             raise Warning("進出貨分類空值")
 
-    plate_no = fields.Char("車號", compute="_auto_fetch_plateno", store= True)
+    plate_no = fields.Char("車號", compute="_auto_fetch_plateno", store=True)
+
     # 選完司機名稱 車牌自動帶入
     @api.depends("driver_id")
     def _auto_fetch_plateno(self):
-        if self.driver_id:
-            self.plate_no = self.env["yc.driver"].search([('name', '=', self.driver_id.name)]).plate_no
-        else:
-            pass
-
+        for rec in self:
+            if rec.driver_id:
+                rec.plate_no = self.env["yc.driver"].search([('name', '=', self.driver_id.name)]).plate_no
+            else:
+                pass
 
     total = fields.Integer("總重 (KG)")
     curbweight = fields.Integer("空車重 (KG)")
@@ -99,7 +105,6 @@ class YcWeight(models.Model):
     refine = fields.Integer("調質重量 (KG)")
     carbur = fields.Float("滲碳單價")
     other1 = fields.Integer("其他重量1")
-
 
     # 一張過磅單 上面的貨物可能含有多家客戶
     customer_detail_ids = fields.One2many("yc.weight.details", "name", "客戶明細")
